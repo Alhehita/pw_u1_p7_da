dca82a616853de5c109b17fa8ddcbd33d4859bfe
Dennisse <daandradem1@uce.edu.ec>
2024-11-17 19:24:24 -0500

    Taller8 desestructuracion de arreglos simples, objetos y arreglos de objetos compuestos

--- js/jsexternolambda.js
+++ js/jsexternolambda.js
@@ -74,11 +74,11 @@ const conceptosJS = () => {
 
     //manejo de objetos
     const miEstudiante = {
-        nombre:'Dennisse',
-        apellido:'Andrade',
-        edad:27,
-        genero:'Femenino',
-        ciudadNAcimienot:'Quito'
+        nombre: 'Dennisse',
+        apellido: 'Andrade',
+        edad: 27,
+        genero: 'Femenino',
+        ciudadNAcimienot: 'Quito'
     }
 
     console.log(miEstudiante);
@@ -86,7 +86,7 @@ const conceptosJS = () => {
     console.log(miEstudiante.apellido);
     console.log(miEstudiante.edad);
 
-    if(miEstudiante.edad === 28){
+    if (miEstudiante.edad === 28) {
         console.log('Tiene 28');
     } else {
         console.log('Aun no tiene 28');
@@ -98,11 +98,11 @@ const conceptosJS = () => {
     //declaraciones de atributos pero como objetos
     const miProfesor = {
         nombre: 'Alexandra',
-        apellido:'Andrade',
-        direccion:{
+        apellido: 'Andrade',
+        direccion: {
             calle: "Av. America",
-            casa:'185',
-            barrio:'belisario quevedo'
+            casa: '185',
+            barrio: 'belisario quevedo'
         }
     }
 
@@ -113,9 +113,163 @@ const conceptosJS = () => {
 
     console.log(miProfesor);
 
-    if(miProfesor.edad === miProfesor.estatura){
+    if (miProfesor.edad === miProfesor.estatura) {
         console.log('son iguales');
     } else {
         console.log('estos no son iguales');
     }
+
+    //arreglo de objetos
+
+    const est1 = {
+        nombre: 'Dennisse',
+        apellido: 'Andrade',
+        edad: 27
+    }
+
+    const est2 = {
+        nombre: 'Diego',
+        apellido: 'Rivas',
+        edad: 27
+    }
+
+    const est3 = {
+        nombre: 'Mauricio',
+        apellido: 'Cacuango',
+        edad: 24
+    }
+
+    const arregloEstu = [est1, est2, est3];
+
+    console.log(arregloEstu);
+
+    console.log(arregloEstu[0]);
+    console.log(arregloEstu[1].nombre);
+
+    const arregloEstu2 = [{
+        nombre: 'Alexandra',
+        apellido: 'Marin',
+        edad: 27
+    }, {
+        nombre: 'Alexander',
+        apellido: 'Haro',
+        edad: 27
+    }, {
+        nombre: 'Alexander',
+        apellido: 'Alban',
+        edad: 24
+    }];
+
+    console.log(arregloEstu2);
+
+    console.table(arregloEstu2)
+
+    const estudiante = arregloEstu2.pop(); //elimina el ultimo elemento del array
+
+    console.log(estudiante);
+
+
+    console.table(arregloEstu2);
+
+
+    //desestructuracion de arreglos
+
+    console.log('desestructuracion de arreglos')
+
+    const colores = ['amarillo', 'azul', 'rojo'];
+
+    const [col1, col2, col3] = colores;
+
+    console.log(col1);
+    console.log(col2);
+    console.log(col3);
+
+    const [m1, m2, m3, m4, m5, m6] = ['enero', 'febrero', 'marzo', 'abril', 'mayo'];
+
+    console.log(m1);
+    console.log(m2);
+    console.log(m3);
+    console.log(m4);
+    console.log(m5);
+    console.log(m6);
+
+    const [, mes2, , , mes5] = ['enero', 'febrero', 'marzo', 'abril', 'mayo'];
+
+    //desestructuracion de objetos
+
+    console.log('desestructuracion de objetos')
+    const auto = {
+        marca: 'toyota',
+        modelo: 'piur',
+        color: 'plateado',
+        anio: 2024
+    }
+
+    // const { modelo, color, anio, marca} = auto; // viene dada por le nombre del atributo no por la posicion
+
+
+
+
+    console.log('desestructuracion de objetos')
+    const auto2 = {
+        marca: 'toyota',
+        modelo: 'piur',
+        color: 'plateado',
+        anio: 2024
+    }
+
+    desestructuracion(auto);
+    desestructuracion(auto2);
+
+    const { nomP, raza, peso } = { nombre: 'perro', raza: 'pastor', peso: 15 };
+
+    console.log(nomP);
+    console.log(raza);
+    console.log(peso);
+
+
+    //declarar un arreglo de objetos
+
+    //desestructurar el atributo de objeto del arreglo
+
+    const arregloObj = {
+        nombre: 'Justin',
+        caracteristicas: {
+            peso: 15,
+            edad: 13,
+            tipo: 'perro'
+        }
+    }
+    ;
+
+    /*
+const {animal, caracteristicas} = arregloObj;
+const{tipo}=caracteristicas;
+
+console.log(animal);
+console.log(caracteristicas);
+console.log(tipo);
+*/
+
+    const { animal, caracteristicas: { tipo } } = arregloObj;
+
+    console.log(tipo);
 };
+
+
+
+const desestructuracion = ({ modelo, color, anio, marca }) => {
+    console.log('entro al metodo de desustructuracion')
+
+    console.log(marca);
+    console.log(color);
+    console.log(anio);
+    console.log(modelo);
+};
+
+
+
+
+
+
+
